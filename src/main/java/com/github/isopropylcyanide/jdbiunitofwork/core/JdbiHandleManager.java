0171f89e57c605e214016250f96a8cf886e4be9d
Aman Garg <amangargcse@outlook.com>
2020-04-25 17:42:27 +0530

    Enhance javadocs

--- src/main/java/com/github/isopropylcyanide/jdbiunitofwork/core/JdbiHandleManager.java
+++ src/main/java/com/github/isopropylcyanide/jdbiunitofwork/core/JdbiHandleManager.java
@@ -1,9 +1,26 @@
+/*	/*
+ * Licensed under the Apache License, Version 2.0 (the "License");	 * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	 * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at	 * You may obtain a copy of the License at
+ *	 *
+ * http://www.apache.org/licenses/LICENSE-2.0	 * http://www.apache.org/licenses/LICENSE-2.0
+ *	 *
+ * Unless required by applicable law or agreed to in writing, software	 * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,	 * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and	 * See the License for the specific language governing permissions and
+ * limitations under the License.	 * limitations under the License.
+ */
+
 package com.github.isopropylcyanide.jdbiunitofwork.core;
 
 import org.skife.jdbi.v2.Handle;
 
 import java.util.concurrent.ThreadFactory;
 
+/**
+ * A {@link JdbiHandleManager} is used to manage the lifecycle of a {@link Handle}
+ */
 public interface JdbiHandleManager {
 
     /**
@@ -17,7 +34,13 @@ public interface JdbiHandleManager {
     void clear();
 
     /**
-     * Provide a thread factory for the caller with some identity.
+     * Provide a thread factory for the caller with some given identity. This can be used by the
+     * caller to create multiple threads, say, using {@link java.util.concurrent.ExecutorService}
+     * with a custom thread factory. The {@link JdbiHandleManager} can then use the thread factory
+     * to identify and manage handle use across multiple threads.
+     * <p>
+     * This feature should be supported in only those implementations of {@link JdbiHandleManager}
+     * which allow the handle to be safely used across multiple threads.
      *
      * @param threadIdentity a unique identifier for the calling thread
      * @return the default thread factory in Executors
--- src/main/java/com/github/isopropylcyanide/jdbiunitofwork/core/JdbiTransactionAspect.java
+++ src/main/java/com/github/isopropylcyanide/jdbiunitofwork/core/JdbiTransactionAspect.java
@@ -1,12 +1,28 @@
-package com.github.isopropylcyanide.jdbiunitofwork.core;
+/*	/*
+ * Licensed under the Apache License, Version 2.0 (the "License");	 * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	 * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at	 * You may obtain a copy of the License at
+ *	 *
+ * http://www.apache.org/licenses/LICENSE-2.0	 * http://www.apache.org/licenses/LICENSE-2.0
+ *	 *
+ * Unless required by applicable law or agreed to in writing, software	 * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,	 * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and	 * See the License for the specific language governing permissions and
+ * limitations under the License.	 * limitations under the License.
+ */
 
+package com.github.isopropylcyanide.jdbiunitofwork.core;
 
 import lombok.extern.slf4j.Slf4j;
 import org.skife.jdbi.v2.Handle;
 
 /**
- * An aspect providing operations around a method with the {@link com.github.isopropylcyanide.jdbiunitofwork} annotation.
- * It optionally opens a transaction. It should be created for every invocation of the resource method
+ * An aspect providing low level operations around a {@link Handle}
+ * This is inspired from Dropwizard's Unit of work aspect used to manage handles for hibernate.
+ *
+ * @see <a href="https://github.com/dropwizard/dropwizard/blob/master/dropwizard-hibernate/src/main/java/io/dropwizard/hibernate/UnitOfWorkAspect.java">
+ * Unit Of Work Aspect</a>
  */
 @Slf4j
 public class JdbiTransactionAspect {
