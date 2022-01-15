50abd131a60a8af8ecab592e1ef8b5554edbc98b
Aman Garg <amangarg1995sep@gmail.com>
2022-01-15 17:01:06 +0530

    Add JDBI Unit of Work Provider

--- example/src/test/java/com/github/isopropylcyanide/example/app/JdbiUnitOfWorkModuleTest.java
+++ example/src/test/java/com/github/isopropylcyanide/example/app/JdbiUnitOfWorkModuleTest.java
@@ -1,24 +1,21 @@
 package com.github.isopropylcyanide.example.app;
 
+import com.github.isopropylcyanide.example.app.dao.CountingDao;
+import com.github.isopropylcyanide.example.app.resource.CountingResource;
 import com.github.isopropylcyanide.jdbiunitofwork.core.JdbiHandleManager;
-import com.google.common.collect.Sets;
 import com.google.inject.ConfigurationException;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import jersey.repackaged.com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Test;
-import org.skife.jdbi.v2.Handle;
-import org.skife.jdbi.v2.sqlobject.SqlQuery;
-import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
-import java.util.Set;
+import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@SuppressWarnings("unused")
 public class JdbiUnitOfWorkModuleTest {
 
     private Injector injector;
@@ -26,31 +23,14 @@ public class JdbiUnitOfWorkModuleTest {
     @Before
     public void setUp() {
         JdbiHandleManager mockHandleManager = mock(JdbiHandleManager.class);
-        when(mockHandleManager.get()).thenReturn(mock(Handle.class));
-        Set<String> daoPackages = Sets.newHashSet("com.github.isopropylcyanide.example.app");
+        List<String> daoPackages = Lists.newArrayList("com.github.isopropylcyanide.example.app");
         JdbiUnitOfWorkModule module = new JdbiUnitOfWorkModule(mockHandleManager, daoPackages);
         injector = Guice.createInjector(module);
     }
 
     @Test
-    public void testModuleBindsTheProxiedDao() {
-        assertNotNull(injector.getInstance(DaoA.class));
-        assertNotNull(injector.getInstance(DaoB.class));
-        assertThrows(ConfigurationException.class, () -> injector.getInstance(DaoC.class));
-    }
-
-    interface DaoA {
-
-        @SqlUpdate
-        void update();
-    }
-
-    interface DaoB {
-
-        @SqlQuery
-        void select();
-    }
-
-    interface DaoC {
+    public void testModuleBindsTheProxiedDaoButFailsForOther() {
+        assertNotNull(injector.getInstance(CountingDao.class));
+        assertThrows(ConfigurationException.class, () -> injector.getInstance(CountingResource.class));
     }
 }
